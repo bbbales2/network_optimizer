28483756c37540c6501b3f218d5c5bd923a6517a
Ben Bales <bbbales2@gmail.com>
2016-02-28 10:38:25 -0800

    Made some changes to the acyclic game

--- acyclic_game_NE.py
+++ acyclic_game_NE.py
@@ -5,7 +5,7 @@ import networkx
 
 
 
-def runRound(G, start, end, cost, tax, bias, N): 
+def runRound(G, start, end, cost, tax, bias, N):
 
     # For each edge there is flow * c(flow). These are the edgeFuncs. The global
     #   optimizer minimizes the sum of these functions
@@ -28,27 +28,30 @@ def runRound(G, start, end, cost, tax, bias, N):
         for n2 in G.edge[n1]:
             for en in G.edge[n1][n2]:
                 G.edge[n1][n2][en]['f'] = 0.0
-                
+
     # we need to preserve the balance of edges at the end of the round. becuas I should change my mind just because the flow changed...
     # If I change my mind, remove myself from the previous
     # for r in range(R):
-    aCur ={} # create 
-    aPrev =None
-    ct=0
-    history=[]
-    backtrack =False
-    while ((initial or aCur != aPrev) and ct <100):
-        ct+=1
+    aCur = {} # create
+    aPrev = None
+    ct = 0
+    history = []
+    backtrack = False
+    while ((initial or aCur != aPrev) and ct < 100):
+        ct += 1
+        print 'hi'
         if aCur in history:
             backtrack=True
         history.append(dict(aCur))
         aPrev = dict(aCur)
 
-
-
         order = range(N)
         numpy.random.shuffle(order)
         for i in order:
+            if i in aPrev:
+                for n1, n2, betterEdgeIdx in aCur[i]:
+                    G.edge[n1][n2][betterEdgeIdx]['f'] -= 1.0 / N
+
             for n1 in G.edge:
                 for n2 in G.edge[n1]:
                     for en in G.edge[n1][n2]:
@@ -60,42 +63,46 @@ def runRound(G, start, end, cost, tax, bias, N):
 
             pEdge=list(zip(path[:-1], path[1:]))
             pEdge.sort()
-            aCur[i] =pEdge
-            if (not initial):
-                if(aPrev[i] !=pEdge):
-                    for eo, en in zip(aPrev[i],pEdge):
-                        if (eo != en):
-                            sorted(G.edge[en[0]][en[1]].values(), key = lambda x : x['c'])[0]['f'] += 1.0 / N
-                            sorted(G.edge[eo[0]][eo[1]].values(), key = lambda x : x['c'])[0]['f'] -= 1.0 / N
-            else:
-                for n1, n2 in pEdge: #loop over the path increasing flow
-                    sorted(G.edge[n1][n2].values(), key = lambda x : x['c'])[0]['f'] += 1.0 / N
 
-        initial =False  
+            fullEdges = []
+            for n1, n2 in pEdge: #loop over the path increasing flow
+            # It's possible there are two paths from one node to another, we must choose the better one
+                betterEdgeIdx, betterEdgeData = sorted(G.edge[n1][n2].items(), key = lambda x : x[1]['c'])[0]
 
-    totalc = 0.0
-    for n1, n2 in set(G.edges()):
-        for edgeData in G.get_edge_data(n1, n2).values():
-            key = (n1, n2, edgeData['t'])
-            if key not in edgeResults:
-                edgeResults[key] = []
+                fullEdges.append((n1, n2, betterEdgeIdx))
 
-            edgeResults[key].append(edgeData['f'])
-            totalc += edgeData['f'] * cost[edgeData['t']](edgeData['f'])
+            aCur[i] = fullEdges
 
-    #totals.append(totalc)
+            for n1, n2, betterEdgeIdx in aCur[i]:
+                G.edge[n1][n2][betterEdgeIdx]['f'] += 1.0 / N
 
+            totalc = 0.0
+            for n1, n2 in set(G.edges()):
+                for edgeIdx, edgeData in G.edge[n1][n2].items():
+                    key = (n1, n2, edgeIdx)
 
+                    edgeResults[key] = edgeData['f']
+                    totalc += edgeData['f'] * cost[edgeData['t']](edgeData['f'])
 
-    for (n1, n2, t), f in edgeResults.items():
-        print u"Edge ({0}, {1}), type {2}, flow {3} \u00B1 {4}".format(n1, n2, t, numpy.mean(f), numpy.std(f))
+            for (n1, n2, t), f in edgeResults.items():
+                print u"Edge ({0}, {1}), type {2}, flow {3}".format(n1, n2, t, f)
 
-    print '----'
-    print u"Total cost: {0} \u00B1 {1}".format(numpy.mean(totalc), numpy.std(totalc))
-    print 'Total iterations:', ct
-    print 'The game backtracked and therefore is not gaurnteed to be NE:', backtrack
+            print '----'
+            print u"Total cost: {0}".format(numpy.mean(totalc))
+            print '****'
+        initial = False
+        print 'Total iterations:', ct
 
-bias =numpy.random.exponential
+    print aCur, aPrev
+    print initial
+    print aCur != aPrev
+    print ct
+    if backtrack:
+        print "Game is not potential game"
+    else:
+        print "No backtracking detected"
+
+bias = lambda : 0#numpy.random.exponential
 #bias = lambda : 0
 G = networkx.MultiGraph()
 edges = [(0, 1, { 'f' : 0.0, 'c' : 0.0, 't' : 0 }),
@@ -108,7 +115,7 @@ edges = [(0, 1, { 'f' : 0.0, 'c' : 0.0, 't' : 0 }),
 
 G.add_edges_from(edges)
 
-cost = [lambda x : x, lambda x : 1.01, lambda x : 0]
+cost = [lambda x : x, lambda x : 1.00, lambda x : 0]
 tax = [lambda x : 0, lambda x : 0, lambda x : 0]
 
 start = 0
